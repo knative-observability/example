b2d5d89c79582a2ebc348d2b6817a258690dda04
Yuezheng Liu <i@lyz.plus>
2024-05-05 15:50:47 +0000

    sine wave test

--- test/locustfile.py
+++ test/locustfile.py
@@ -1,5 +1,6 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, LoadTestShape
 import random
+import math
 
 class MyUser(HttpUser):
     wait_time = between(1, 3)
@@ -19,3 +20,19 @@ class MyUser(HttpUser):
         }
 
         self.client.post("/", params=params)
+
+class SineLoadShape(LoadTestShape):
+    period = 60
+    min_users = 10
+    max_users = 50
+
+    def tick(self):
+        run_time = self.get_run_time()
+        user_range = self.max_users - self.min_users
+        amplitude = user_range / 2
+        average_users = self.min_users + amplitude
+        user_count = round(average_users + amplitude * math.sin(2 * math.pi * run_time / self.period))
+        spawn_rate = user_count - self.get_current_user_count()
+        if spawn_rate == 0:
+            spawn_rate = 1
+        return user_count, spawn_rate
